b326baf5eeabed4d56a53021802bb6bd5c07567c
brunanunes <brunanunes324@gmail.com>
2023-12-21 13:00:56 -0300

    second commit

--- batalha.py
+++ batalha.py
@@ -1,19 +1,24 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 import random
+import sys
  
 class telas:
     def __init__(self):
-        app = QtWidgets.QApplication([])
- 
+        self.app = QtWidgets.QApplication([])
         self.telaInicial = uic.loadUi("telas/inicial.ui")
         self.tabuleiro = uic.loadUi("telas/jogo.ui")
         self.telaGameOver = uic.loadUi("telas/perdeu.ui")
         self.telaWinner = uic.loadUi("telas/vencedor.ui")
         self.telaInicial.show()
         self.telaInicial.start.clicked.connect(self.mudar_telaInicial)
+        self.telaInicial.Exit.clicked.connect(self.fechar_tela)
+        self.telaGameOver.sair.clicked.connect(self.fechar_tela)
+        self.telaWinner.sairGanhou.clicked.connect(self.fechar_tela)
+        self.telaGameOver.againGameOver.clicked.connect(self.again)
+        self.telaWinner.again.clicked.connect(self.again)
         self.tentativas_max = 6
         self.tentativas = 0
- 
+
         coordenadasLetras = ["A", "B", "C", "D", "E"]
         coordenadasNumeros = ["1","2","3","4","5"]
         sorteio = []
@@ -71,13 +76,32 @@ class telas:
             button.clicked.connect(self.selecionarBotao)
                    
            
-           
- 
-        app.exec()
+        self.app.exec()
+        sys.exit(self.app.exec_())
+
+    def restart(self):
+        QtCore.QCoreApplication.quit() #permite sair da aplicação
+        QtCore.QProcess.startDetached(sys.executable, sys.argv) # permite que o programa incie novamente antes de sair por completo da aplicação
+
+    def again(self):
+        self.telaGameOver.close()
+        self.telaWinner.close()
+        self.restart()
+            
  
     def mudar_telaInicial(self):
         self.telaInicial.close()
         self.tabuleiro.show()
+    
+    def fechar_tela(self):
+        self.telaGameOver.close()
+        self.telaWinner.close()
+        self.telaInicial.close()
+        exit()
+    
+    # def again(self):
+    #     self.telaGameOver.close()
+    #     self.telaInicial.show()
  
  
    
